c884d3c77dc5ec0c5d3414dfc28d8e20d0eb857c
real <real@freedomlayer.org>
2017-08-19 14:48:35 +0300

    Added a fix to starting nautilus on i3.

--- installation.sh
+++ installation.sh
@@ -1,6 +1,12 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
+
+# Exit on error
+set -e 
 
 # Required packages for the i3 configuration to work:
 
 # Used for capturing a screen shot (Used for locking the screen):
 apt install scrot xautolock
+
+# Configure nautilus to work with i3:
+gsettings set org.gnome.desktop.background show-desktop-icons false
