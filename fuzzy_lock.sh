32f24f1a58a3e1c90993121049340b789f4a8a6c
real <real@freedomlayer.org>
2017-08-19 13:20:08 +0300

    Initial commit for i3 configuration.

---
+++ fuzzy_lock.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+# This script was taken from: 
+# https://faq.i3wm.org/question/83/
+#       how-to-run-i3lock-after-computer-inactivity.1.html
+
+# Take a screenshot
+scrot /tmp/screen_locked.png
+
+# Pixelalte it 10x
+mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+
+# Lock screen displaying this image
+i3lock -i /tmp/screen_locked.png
+
+# Turn the scree off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off
---
+++ installation.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+# Required packages for the i3 configuration to work:
+
+# Used for capturing a screen shot (Used for locking the screen):
+apt install scrot
