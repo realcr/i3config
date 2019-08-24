9ad33a62c37c714a14422a9094f01208262365c0
real <real@freedomlayer.org>
2019-08-24 23:14:11 +0300

    Adaptations to ubuntu18.

--- installation.sh
+++ installation.sh
@@ -18,5 +18,18 @@ apt install xfce4-power-manager-settings
 # Install redshift, to control monitor color
 apt install redshift redshift-gtk
 
-# Configure nautilus to work with i3:
+# Configure nautilus to work with i3 (Ubuntu 16?)
 gsettings set org.gnome.desktop.background show-desktop-icons false
+
+# Add missing icons to nautilus:
+# See: https://askubuntu.com/questions/319547/some-icons-missing-in-nautilus
+apt install gnome-icon-theme-symbolic
+
+# Install fcitx (keyboard input):
+# Remember to edit ~/.pam_environment on gnome (See resources dir)
+apt install fcitx-bin fcitx-googlepinyin
+
+# Used to configure GUI to look like out of the box Ubuntu 18:
+# Run gnome-tweaks later to choose themes.
+sudo apt install gnome-tweak-tool
+
