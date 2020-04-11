b286cd2efc375ac829ae92849e9a4caa2ab05095
real <real@freedomlayer.org>
2020-04-11 11:57:17 +0300

    Remapped right winkey to be i3 super key

--- map_extra_mod2.sh
+++ map_extra_mod2.sh
@@ -1,6 +1,6 @@
 # Remap right win key to be left win key
-# xmodmap -e 'keycode 135 = Super_R' && xset -r 135
+xmodmap -e 'keycode 135 = Super_R' && xset -r 135
 # Remap web next key to be win key:
-xmodmap -e 'keycode 167 = Super_R' && xset -r 167
+# xmodmap -e 'keycode 167 = Super_R' && xset -r 167
 # Remap web prev key to be win key:
-xmodmap -e 'keycode 166 = Super_R' && xset -r 166
+# xmodmap -e 'keycode 166 = Super_R' && xset -r 166
