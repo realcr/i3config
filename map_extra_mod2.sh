7b6f4ca2e2a1a5d9c1e2d317b9c7d0db83e08975
real <real@freedomlayer.org>
2020-04-11 11:32:58 +0300

    map web next/prev keys to be super keys

--- map_extra_mod2.sh
+++ map_extra_mod2.sh
@@ -1,1 +1,6 @@
-xmodmap -e 'keycode 135 = Super_R' && xset -r 135
+# Remap right win key to be left win key
+# xmodmap -e 'keycode 135 = Super_R' && xset -r 135
+# Remap web next key to be win key:
+xmodmap -e 'keycode 167 = Super_R' && xset -r 167
+# Remap web prev key to be win key:
+xmodmap -e 'keycode 166 = Super_R' && xset -r 166
